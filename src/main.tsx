b853d2acd766477f363f74766dcc6fc44a85d3db
Egor Tsukanov <ts.eg.99@gmail.com>
2025-02-14 11:31:21 +0100

    journal save

--- src/main.tsx
+++ src/main.tsx
@@ -7,23 +7,38 @@ createRoot(document.getElementById("root")!).render(
   // </StrictMode>,
 );
 
-// Date: 13.02.2025, 16:35:08
-// Subject: Legato 3412
-// BPM: 100
-// Rating: 27
-// Comment: too much noise
-// Date: 13.02.2025, 16:41:33
-// Subject: Legato 1-2-4
-// BPM: 100
-// Rating: 68
-// Comment: good
-// Date: 13.02.2025, 16:48:34
-// Subject: Treli #1
-// BPM: 100
-// Rating: 77
-// Comment: almost clean upper strings are make too much noise
-// Date: 13.02.2025, 16:57:54
-// Subject: Treli #2
-// BPM: 100
-// Rating: 68
-// Comment: в райне кистевых связок рука как будто забилаcь, дико устала. Спускаться практически очень легко, подниматься в несколько раз тяжелее
+// const j = [
+//   {
+//     date: "13.02.2025, 16:35:08",
+//     technique: "Legato",
+//     pattern: "2-4-1-3",
+//     bpm: 100,
+//     rating: 27,
+//     comment: "too much noise",
+//   },
+//   {
+//     date: "13.02.2025, 16:41:33",
+//     technique: "Legato",
+//     pattern: "1-2-4",
+//     bpm: 100,
+//     rating: 68,
+//     comment: "good",
+//   },
+//   {
+//     date: "13.02.2025, 16:48:34",
+//     technique: "Legato",
+//     pattern: "Treli #1",
+//     bpm: 100,
+//     rating: 77,
+//     comment: "almost clean upper strings are make too much noise",
+//   },
+//   {
+//     date: "13.02.2025, 16:57:54",
+//     technique: "Legato",
+//     pattern: "Treli #2",
+//     bpm: 100,
+//     rating: 68,
+//     comment:
+//       "в райне кистевых связок рука как будто забилаcь, дико устала. Спускаться практически очень легко, подниматься в несколько раз тяжелее",
+//   },
+// ];
